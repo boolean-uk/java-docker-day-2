2e0614d98dd19d23e83d1c24ef2b59165441e6f3
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-19 16:31:49 +0100

    Create like endpoints

--- src/main/java/com/booleanuk/api/controller/LikeController.java
+++ src/main/java/com/booleanuk/api/controller/LikeController.java
@@ -1,8 +1,16 @@
 package com.booleanuk.api.controller;
 
-import com.booleanuk.api.payload.response.Response;
+import com.booleanuk.api.model.Like;
+import com.booleanuk.api.model.Post;
+import com.booleanuk.api.model.User;
+import com.booleanuk.api.model.UserPostKey;
+import com.booleanuk.api.payload.response.*;
 import com.booleanuk.api.repository.LikeRepository;
+import com.booleanuk.api.repository.PostRepository;
+import com.booleanuk.api.repository.UserFollowRepository;
+import com.booleanuk.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +20,66 @@ public class LikeController {
     @Autowired
     LikeRepository repository;
 
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
 
     @GetMapping("/posts/{postId}")
-    public ResponseEntity<Response<?>> getLikesForPosts() {return null;}
+    public ResponseEntity<Response<?>> getLikesForPost(@PathVariable int postId) {
+        int likes;
+        try {
+            likes = this.repository.findAmountOfLikesOnPost(postId);
+        } catch (Exception e) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        StringResponse stringResponse = new StringResponse();
+        stringResponse.set("Post has "+likes+" likes");
+        return ResponseEntity.ok(stringResponse);
+    }
 
     @PostMapping("/posts/{postId}/users/{userId}")
-    public ResponseEntity<Response<?>> likePost() {return null;}
+    public ResponseEntity<Response<?>> likePostForUser(@PathVariable int postId, @PathVariable int userId) {
+        Post post = this.postRepository.findById(postId).orElse(null);
+        User user = this.userRepository.findById(userId).orElse(null);
+        if (post == null || user == null) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        Like like = new Like(user, post);
+        UserPostKey userPostKey = new UserPostKey();
+        userPostKey.setPostId(post.getId());
+        userPostKey.setUserId(user.getId());
+        like.setId(userPostKey);
+
+        LikeResponse likeResponse = new LikeResponse();
+        try {
+            likeResponse.set(this.repository.save(like));
+        } catch (Exception e) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("Bad request");
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(likeResponse, HttpStatus.CREATED);
+    }
 
     @DeleteMapping("/posts/{postId}/users/{userId}")
-    public ResponseEntity<Response<?>> unlikePost() {return null;}
+    public ResponseEntity<Response<?>> unlikePostForUser(@PathVariable int postId, @PathVariable int userId) {
+        Like likeToDelete = this.repository.findByUserIdAndPostId(userId, postId).orElse(null);
+        if ( likeToDelete == null) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        this.repository.delete(likeToDelete);
+        LikeResponse likeResponse = new LikeResponse();
+        likeResponse.set(likeToDelete);
+        return ResponseEntity.ok(likeResponse);
+    }
 }
--- src/main/java/com/booleanuk/api/repository/LikeRepository.java
+++ src/main/java/com/booleanuk/api/repository/LikeRepository.java
@@ -1,7 +1,15 @@
 package com.booleanuk.api.repository;
 
 import com.booleanuk.api.model.Like;
+import com.booleanuk.api.model.UserPostKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface LikeRepository extends JpaRepository<Like, Integer> {
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, UserPostKey> {
+    @Query("SELECT COUNT(*) FROM Like l JOIN l.post p WHERE p.id = :postId")
+    int findAmountOfLikesOnPost(int postId);
+
+    Optional<Like> findByUserIdAndPostId(int userId, int postId);
 }
