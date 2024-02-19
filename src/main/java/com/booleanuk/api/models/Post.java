36483ba1926587f6a3565e28156d0336943cad8c
Robin Kaastrup <robin.kaastrup@gmail.com>
2024-02-19 13:31:30 +0100

    Endpoints should work now. They're quite minimal as i will be focusing on getting the containerization working

---
+++ src/main/java/com/booleanuk/api/models/Post.java
@@ -0,0 +1,46 @@
+package com.booleanuk.api.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "likes")
+    private int likes;
+
+    public Post(String content) {
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.likes = 0;
+    }
+
+    public Post(int id) {
+        this.id = id;
+    }
+
+    public void Like() {
+        likes++;
+    }
+}
