07539b03a338aa231b5c7b0c34ea387a45467c76
Ateeb <ateeb@live.no>
2024-02-22 13:44:26 +0100

    Ateeb Salam

---
+++ src/main/java/com/booleanuk/library/payload/response/BlogListResponse.java
@@ -0,0 +1,9 @@
+package com.booleanuk.library.payload.response;
+
+import com.booleanuk.library.models.Blog;
+
+import java.util.List;
+
+public class BlogListResponse extends Response<List<Blog>> {
+
+}
---
+++ src/main/java/com/booleanuk/library/payload/response/PostListResponse.java
@@ -0,0 +1,9 @@
+package com.booleanuk.library.payload.response;
+
+import com.booleanuk.library.models.Post;
+
+import java.util.List;
+
+public class PostListResponse extends Response<List<Post>> {
+
+}
