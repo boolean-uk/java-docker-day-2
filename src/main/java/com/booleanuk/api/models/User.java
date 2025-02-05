a2f6a913712180c51059e1176eae445c74bd3f0d
Magnus-droid <magnuskrumbacher@gmail.com>
2025-02-05 08:39:05 +0100

    Solved core tasks

---
+++ src/main/java/com/booleanuk/api/models/User.java
@@ -0,0 +1,35 @@
+package com.booleanuk.api.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties({"id", "user"})
+    private List<Post> posts;
+
+    public User(int id) {
+        this.id = id;
+    }
+
+
+}
